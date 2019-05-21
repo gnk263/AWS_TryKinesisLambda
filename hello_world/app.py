a6dba595e9c66a8b82382673f777bfd81035abc2
gnk263 <gnk263@users.noreply.github.com>
2019-05-21 10:55:25 +0900

    Kinesisから受け取ったデータをログ出力する

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,8 +1,16 @@
+import base64
 import json
 
 
 def lambda_handler(event, context):
-    print(event)
+    records_count = len(event['Records'])
+    print(f'kinesis recourds count: {records_count}')
+
+    for record in event['Records']:
+        b64_data = record['kinesis']['data']
+        data = base64.b64decode(b64_data).decode('utf-8')
+
+        print(data)
 
     return {
         'statusCode': 200,
